2a41541659e9f03161bb62ca249ff21d1595d85f
jas <bigkingjohn@hotmail.com>
2017-02-07 21:08:37 +0000

    bare init

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var liveServer = require('live-server');
+
+gulp.task('serve-dev', [], function () {
+    var params = {
+        port: 8080,
+        host: "localhost",
+        root: "./www/",
+        open: true,
+        file: "index.html",
+        wait: 500
+    };
+
+    liveServer.start(params);
+})
