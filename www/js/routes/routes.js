fdb8f3bf833d6856c00cf0beec4a949abee8855a
jas <bigkingjohn@hotmail.com>
2017-02-16 23:33:31 +0000

    Can solve the multipart UI by using UI Router views - essentially state (page) renders it's own header/footer view.  meaning the controllers are created from scratch each time.  I feel like that's not the ideal solution. why do I have to render it once for every page.  but it looks like my other options are to put the header and footers into index.html but outside the scope of the ui-view.
    First of these is what another tutorial suggested, but he created directives for his headers/footers.  i had some success using a directive, but didnt get it doing anything and couldnt get the attributes to work (scope: "=").
    https://gon.to/2013/03/23/the-right-way-of-coding-angularjs-how-to-organize-a-regular-webapp/

--- www/js/routes/routes.js
+++ www/js/routes/routes.js
@@ -3,71 +3,44 @@ angular.module('homs')
         $stateProvider
             .state('root', {
                 url: '/',
-                templateUrl: 'js/navigation/body.tmpl.html',
-                // views: {
-                //     'header': {
-                //         templateUrl: 'js/navigation/header.tmpl.html',
-                //         // controller: 'headerController',
-                //         // controllerAs: 'vm'
-                //     },
-                //     'body': {
-                //         templateUrl: 'js/navigation/body.tmpl.html',
-                //         // controller: 'bodyController',
-                //         // controllerAs: 'vm'
-                //     },
-                //     'footer': {
-                //         templateUrl: 'js/navigation/footer.tmpl.html',
-                //         // controller: 'footerController',
-                //         // controllerAs: 'vm'
-                //     }
-                // }
+                views: {
+                    'header': {
+                        templateUrl: 'js/navigation/header.tmpl.html',
+                        // controller: 'headerController',
+                        // controllerAs: 'vm'
+                    },
+                    'body': {
+                        templateUrl: 'js/navigation/body.tmpl.html',
+                        // controller: 'bodyController',
+                        // controllerAs: 'vm'
+                    },
+                    'footer': {
+                        templateUrl: 'js/navigation/footer.tmpl.html',
+                        // controller: 'footerController',
+                        // controllerAs: 'vm'
+                    }
+                }
             })
-            // .state('project', {
-            //     views: {
-            //         'project@': {
-            //            url: '/project',
-            //            templateUrl: 'js/project/project.tmpl.html',
-            //         }
-            //     }
-            // });
             .state('project', {
-                parent: 'root',
-                templateUrl: 'js/project/project.tmpl.html',
-                // views: {
-                //     'project' : {
-                //     }
-                // }
+                url: '/p',
+                views: {
+                    'header': {
+                        templateUrl: 'js/navigation/header.tmpl.html',
+                        // controller: 'headerController',
+                        // controllerAs: 'vm'
+                    },
+                    'body': {
+                        templateUrl: 'js/project/project.tmpl.html',
+                        // controller: 'bodyController',
+                        // controllerAs: 'vm'
+                    },
+                    'footer': {
+                        templateUrl: 'js/navigation/footer.tmpl.html',
+                        // controller: 'footerController',
+                        // controllerAs: 'vm'
+                    }
+                }
             });
-            // .state('header', {
-            //     name: 'headers...',
-            //     url: '/header',
-            //     templateUrl: 'js/navigation/header.tmpl.html'
-
-            // })
-            // .state('footer', {
-            //     name: 'footers...',
-            //     url: '/footer',
-            //     templateUrl: 'js/navigation/footer.tmpl.html'
-            // });;
-
-            // // setup an abstract state for the tabs directive
-            // .state('header', {
-            //     url: '/header',
-            //     abstract: true,
-            //     templateUrl: 'navigation/header.tmpl.html'
-            // })
-            // // Each tab has its own nav history stack:
-            // .state('login', {
-            //     url: '/login',
-            //     views: {
-            //         'tab-login': {
-            //             // template: '<html><body><p>login</p><p>login</p><p>login</p><body></html>',
-            //             templateUrl: 'src/login/login.html',
-            //             controller: 'loginController',
-            //             controllerAs: 'vm'
-            //       }
-            //     }
-            // });
 
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/');
